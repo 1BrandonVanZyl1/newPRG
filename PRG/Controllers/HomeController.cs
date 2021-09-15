7f82f68b0e17780be57911d53a6e84302cdebe4f
1BrandonVanZyl1 <89069026+1BrandonVanZyl1@users.noreply.github.com>
2021-09-15 10:10:33 +0200

    Add project files.

---
+++ PRG/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PRG.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+        public ActionResult Services()
+        {
+            ViewBag.Message = "Your services page.";
+
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Portfolio()
+        {  
+           return View();  
+        }
+        public ActionResult fail()
+        {
+            ViewBag.Message = "Your fail page.";
+
+            return View();
+        }
+
+
+    }
+}
